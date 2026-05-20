612fc62ab54e5792356c518d595431ee6870c1e2
Rafael Alberto <rafaelalberto160407ao@gmail.com>
2026-05-20 12:10:05 +0100

    Update Ship.java
    set class properties for the ships

--- src/Ship.java
+++ src/Ship.java
@@ -1,3 +1,49 @@
 public class Ship {
-    
+
+    private int size;
+    private int health;
+
+    private int row;
+    private int col;
+
+    private boolean horizontal;
+
+    public Ship(int size, int row, int col, boolean horizontal) {
+
+        this.size = size;
+        this.health = size;
+
+        this.row = row;
+        this.col = col;
+
+        this.horizontal = horizontal;
+    }
+
+    public void hit() {
+        health--;
+    }
+
+    public boolean isSunk() {
+        return health <= 0;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    public int getCol() {
+        return col;
+    }
+
+    public boolean isHorizontal() {
+        return horizontal;
+    }
 }
