603154792f9cbb984411e09696faf73bd63bc07c
Luther13 <tchanekdossantos@gmail.com>
2026-05-20 13:02:13 +0100

    Update Player.java
    Lets see(Luther)

--- src/Player.java
+++ src/Player.java
@@ -1,3 +1,46 @@
+
+import java.util.Scanner;
+
 public class Player {
-    
+
+    private String name;
+    private Board board;
+
+    public Player(String name) {
+
+        this.name = name;
+        this.board = new Board();
+    }
+
+    public String getName() {
+
+        return name;
+    }
+
+    public Board getBoard() {
+
+        return board;
+    }
+
+    // Player attacks another player
+    public void attack(Player enemy, Scanner input) {
+
+        System.out.println("\n" + name + "'s turn");
+
+        System.out.print("Row: ");
+        int row = input.nextInt();
+
+        System.out.print("Column: ");
+        int col = input.nextInt();
+
+        enemy.getBoard().attack(row, col);
+    }
+
+    // Alternates turns automatically
+    public static void nextTurn(Player current, Player enemy, Scanner input) {
+
+        current.attack(enemy, input);
+    }
 }
+```
+
